52f766370fdbd2132aebbe8917fa6057fd57174a
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-09-26 14:20:59 +0800

    Update misc.py
    moved hex command

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import random
 import discord
+from PIL import Image
 from datetime import datetime
 import base64
 import aiohttp
@@ -134,5 +135,47 @@ class Miscellaneous(commands.Cog):
       embed.add_field(name='Output:', value=message)
       await ctx.send(embed=embed)
 
+    @commands.command(aliases=['hex', 'colour'])
+    async def color(self, ctx, inputcolor=''):
+      
+      if inputcolor == '':
+        
+        randgb = lambda: random.randint(0, 255)
+        hexcode = '%02X%02X%02X' % (randgb(), randgb(), randgb())
+        rgbcode = str(tuple(int(hexcode[i:i+2], 16) for i in (0, 2, 4)))
+        heximg = Image.new("RGB", (256, 256), '#' + hexcode)
+        heximg.save("color.png")
+        f = discord.File("color.png", filename="color.png")
+        embed=discord.Embed(title='Color Machine', color=discord.Color.random())
+        embed.set_thumbnail(url="attachment://color.png")
+        embed.add_field(name='RGB', value=f'`{rgbcode}`', inline=False)
+        embed.add_field(name='Hex', value=f'`#{hexcode}`', inline=False)
+        embed.timestamp=datetime.utcnow()
+        await ctx.send(file=f, embed=embed)
+        
+      else:
+        if inputcolor.startswith('#'):
+          hexcode = inputcolor[1:]
+          if len(hexcode) == 8:
+            hexcode = hexcode[:-2]
+          elif len(hexcode) != 6:
+            embed=discord.Embed(description='Make sure you entered the correct code or format. `(#7289DA)`', color=ctx.author.color)
+            return await ctx.send(embed=embed)
+        
+          rgbcode = str(tuple(int(hexcode[i:i+2], 16) for i in (0, 2, 4)))
+          heximg = Image.new("RGB", (64, 64), '#' + hexcode)
+          heximg.save("color.png")
+          embed=discord.Embed(title='Color Machine', color=discord.Color.random())
+          embed.set_thumbnail(url="attachment://color.png")
+          f = discord.File("color.png", filename="color.png")
+          embed.add_field(name='RGB', value=f'`{rgbcode}`', inline=False)
+          embed.add_field(name='Hex', value=f'`#{hexcode}`', inline=False)
+          embed.timestamp=datetime.utcnow()
+          await ctx.send(file=f, embed=embed)
+        
+        else:
+          embed=discord.Embed(description='Make sure you entered the correct code or format. `(#7289DA)`', color=ctx.author.color)
+          await ctx.send(embed=embed)
+          
 def setup(bot):
     bot.add_cog(Miscellaneous(bot))
