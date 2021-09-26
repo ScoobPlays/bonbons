515f22f77e75b1d550b42d566cbe0b4b49e599ab
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-09-26 14:31:32 +0800

    Add files via upload

---
+++ Projects/Python/Age Related Stuff/future_year.py
@@ -0,0 +1,14 @@
+#Inputs
+
+age = (int(input('How old are you? ')))
+year_for_age = (int(input('What year do you wanna go to see your age? ')))
+
+#Variables
+
+year_right_now = (int(2021))
+age_variable_one = year_for_age - year_right_now + age
+
+
+#Print Command
+print(f'You will be {age_variable_one} in {year_for_age}')
+
---
+++ Projects/Python/calculator.py
@@ -0,0 +1,18 @@
+first_input = (int(input('First: ')))
+second_input = (int(input('Second: ')))
+operation_question = input('Operation: ')
+
+if operation_question == '+':
+    print(first_input + second_input)
+
+if operation_question == '-':
+    print(first_input - second_input)
+
+if operation_question == '*':
+    print(first_input * second_input)
+
+if operation_question == '/':
+    print(first_input / second_input)
+
+if operation_question == '//':
+    print(first_input // second_input)
