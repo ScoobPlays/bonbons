84ad322585fa5c222ef4ced0788c289b8dc373fd
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-09-26 14:11:16 +0800

    Update main.py
    remove hex command to cogs

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import discord #imports
 import os
 import platform
 import random
-from PIL import Image
 from discord.ext import commands
 from datetime import datetime
-from server import server
+from keep_alive import keep_alive
+
+# gold = #fff1b6
+# light_blue = #96daff
+
 
 token = os.environ['token']
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'),case_insensitive=True, intents = discord.Intents.all())
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'), case_insensitive=True, intents = discord.Intents.all())
 bot.remove_command("help")
 bot.launch_time = datetime.utcnow() #variables & etc
 
@@ -21,12 +24,14 @@ async def on_ready(): #events
     if filename.endswith('.py'):
       bot.load_extension(f'cogs.{filename[:-3]}')
 
-@bot.command(aliases=['ping', 'uptime'], hidden=True) #stats command for ping & uptime
+@bot.command(aliases=['ping', 'uptime']) #stats command for ping & uptime
 async def stats(ctx):
+
   delta_uptime = datetime.utcnow() - bot.launch_time
   hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
   minutes, seconds = divmod(remainder, 60)
   days, hours = divmod(hours, 24)
+
   embed=discord.Embed(title='Bot Statistics')
   embed.add_field(name='Uptime/Latency', value=f'`{hours}h, {minutes}m, {seconds}s` `{round(bot.latency * 1000)}ms`', inline=False)
   embed.add_field(name='Python', value=f"`{platform.python_version()}`")
@@ -34,47 +39,7 @@ async def stats(ctx):
   embed.timestamp = datetime.utcnow()
   await ctx.send(embed=embed)
 
-@bot.command(aliases=['hex', 'colour'])
-async def color(ctx, inputcolor=''):
-    if inputcolor == '':
-        randgb = lambda: random.randint(0, 255)
-        hexcode = '%02X%02X%02X' % (randgb(), randgb(), randgb())
-        rgbcode = str(tuple(int(hexcode[i:i+2], 16) for i in (0, 2, 4)))
-        heximg = Image.new("RGB", (256, 256), '#' + hexcode)
-        heximg.save("color.png")
-        f = discord.File("color.png", filename="color.png")
-        embed=discord.Embed(title='Color Machine', color=discord.Color.random())
-        embed.set_thumbnail(url="attachment://color.png")
-        embed.add_field(name='RGB', value=f'`{rgbcode}`', inline=False)
-        embed.add_field(name='Hex', value=f'`#{hexcode}`', inline=False)
-        embed.timestamp=datetime.utcnow()
-        await ctx.send(file=f, embed=embed)
-
-    else:
-        if inputcolor.startswith('#'):
-            hexcode = inputcolor[1:]
-            if len(hexcode) == 8:
-                hexcode = hexcode[:-2]
-            elif len(hexcode) != 6:
-              embed=discord.Embed(description='Make sure you entered the correct code or format. `(#7289DA)`', color=ctx.author.color)
-              await ctx.send(embed=embed)
-              return
-            rgbcode = str(tuple(int(hexcode[i:i+2], 16) for i in (0, 2, 4)))
-            heximg = Image.new("RGB", (64, 64), '#' + hexcode)
-            heximg.save("color.png")
-            embed=discord.Embed(title='Color Machine', color=discord.Color.random())
-            embed.set_thumbnail(url="attachment://color.png")
-            f = discord.File("color.png", filename="color.png")
-            embed.add_field(name='RGB', value=f'`{rgbcode}`', inline=False)
-            embed.add_field(name='Hex', value=f'`#{hexcode}`', inline=False)
-            embed.timestamp=datetime.utcnow()
-            await ctx.send(file=f, embed=embed)
-        else:
-          embed=discord.Embed(description='Make sure you entered the correct code or format. `(#7289DA)`', color=ctx.author.color)
-          await ctx.send(embed=embed)
-          return
-
-server() #server function
+keep_alive() #Keep alive method for replit ^^
 os.environ.setdefault('JISHAKU_NO_UNDERSCORE', '1') #Jishaku envs
 os.environ.setdefault('JISHAKU_HIDE', '1')
 bot.load_extension('jishaku')
