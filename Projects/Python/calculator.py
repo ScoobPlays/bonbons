ec119159a194e260a580b9abbb1e4dd29e21f011
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-09-26 14:37:01 +0800

    Update calculator.py

--- Projects/Python/calculator.py
+++ Projects/Python/calculator.py
@@ -3,16 +3,21 @@ second_input = (int(input('Second: ')))
 operation_question = input('Operation: ')
 
 if operation_question == '+':
-    print(first_input + second_input)
+  print(first_input + second_input)
+  pass
 
 if operation_question == '-':
-    print(first_input - second_input)
+  print(first_input - second_input)
+  pass
 
 if operation_question == '*':
-    print(first_input * second_input)
+  print(first_input * second_input)
+  pass
 
 if operation_question == '/':
-    print(first_input / second_input)
+  print(first_input / second_input)
+  pass
 
 if operation_question == '//':
-    print(first_input // second_input)
+  print(first_input // second_input)
+  pass
