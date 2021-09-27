75568e79fd3c2ac7775d6713b45a4411b3ded24f
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-09-27 20:07:02 +0800

    Update error_handling.py
    global error handler

--- cogs/error_handling.py
+++ cogs/error_handling.py
@@ -7,42 +7,60 @@ class Errors(commands.Cog):
   
   @commands.Cog.listener()
   async def on_command_error(self, ctx, error):
+    
+    if hasattr(ctx.command, 'on_error'):
+      return
+      
     if isinstance(error, commands.CommandNotFound): #command not found
-      embed=discord.Embed(title='Command Not Found',url=ctx.message.jump_url, description=error)
+      embed=discord.Embed(
+        title='Command Not Found',
+        url=ctx.message.jump_url,
+        description=f"> {error}"
+        )
+
       await ctx.send(embed=embed)
       
     if isinstance(error, commands.DisabledCommand): #disabled command
-      embed=discord.Embed(title='Command Is Disabled', url=ctx.message.jump_url,description=error)
+      embed=discord.Embed(
+        title='Command Is Disabled', 
+        url=ctx.message.jump_url,
+        description=f"> {error}"
+        )
       await ctx.send(embed=embed)
       
     elif isinstance(error, commands.CommandOnCooldown): #cd error
-      embed=discord.Embed(title='Command On Cooldown', url=ctx.message.jump_url,description=f"Command is on cooldown. Try again in **{error.retry_after}** seconds.")
+      embed=discord.Embed(
+        title='Command On Cooldown', 
+        url=ctx.message.jump_url,
+        description=f"> Command is on cooldown. Try again in **{error.retry_after}** seconds."
+        )
       await ctx.send(embed=embed)
 
     elif isinstance(error, commands.MissingRequiredArgument): #missing arg
-      embed=discord.Embed(title='Missing Required Argument', url=ctx.message.jump_url,description=error)
+      embed=discord.Embed(
+        title='Missing Required Argument',
+        url=ctx.message.jump_url, 
+        description=f"> {error}"
+        )
       await ctx.send(embed=embed)
-
-    elif isinstance(error, commands.TooManyArguments): #too many args
-      embed=discord.Embed(title='Too Many Arguments', url=ctx.message.jump_url,description=error)
-      await ctx.send(embed=embed)
+      ctx.command.has_error_handler()
 
     elif isinstance(error, commands.NotOwner): #owner only error
-      embed=discord.Embed(title='Owner Command', url=ctx.message.jump_url,description=error)
+      embed=discord.Embed(
+        title='Not Owner',
+        url=ctx.message.jump_url,
+        description=f"> {error}"
+        )
       await ctx.send(embed=embed)
 
-    elif isinstance(error, commands.MemberNotFound): #member not found
-      embed=discord.Embed(title='Member Not Found', url=ctx.message.jump_url,description=error)
-      await ctx.send(embed=embed)
-
-    elif isinstance(error, commands.GuildNotFound): #server not found
-      embed=discord.Embed(title='Guild Not found',url=ctx.message.jump_url, description=error)
-      await ctx.send(embed=embed)
 
     else: #unknown error
-      embed=discord.Embed(title='Unknown/Unregistered Error', url=ctx.message.jump_url, description=f"Error: {error}\nName: {ctx.command.name}\nCog: {ctx.cog}")
+      embed=discord.Embed(
+        title='Unknown/Unregistered Error', 
+        url=ctx.message.jump_url, 
+        description=f"> Error: {error}\n> Name: {ctx.command.name or None}\n> Cog: {ctx.cog}"
+        )
       await ctx.send(embed=embed)
       
-
 def setup(bot):
   bot.add_cog(Errors(bot))
