649dbd7998b587d954a8a1444518470faa3aa048
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-09-30 12:01:35 +0800

    Update moderation.py
    removed mute/unmute command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -8,7 +8,7 @@ class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['nickname', 'n'])
+    @commands.command(aliases=['nickname'])
     @commands.has_permissions(manage_nicknames=True)
     async def nick(self, ctx, member: commands.MemberConverter=None, *, nick):
      
@@ -28,10 +28,9 @@ class Moderation(commands.Cog):
        embed.timestamp = datetime.utcnow() 
        await ctx.send(embed=embed)
 
-    @commands.command(aliases=['clear', 'clean'])
+    @commands.command(aliases=['clear', 'clean'], help='Purges an amount of messages for you.')
     @commands.has_permissions(manage_messages=True)
     async def purge(self, ctx, amount:int = 2):
-      
       await ctx.channel.purge(limit=amount)
 
     @commands.command(help='Adds/Removes a role from a member.')
@@ -55,7 +54,7 @@ class Moderation(commands.Cog):
     #  Removed Commands  #
     ######################
 
-    @commands.command(help="Mutes the specified user!")
+    @commands.command(help="Mutes the specified user!", hidden=True)
     @commands.is_owner()
     async def mute(self,ctx, member: commands.MemberConverter, *, reason=None):
       guild = ctx.guild
@@ -70,32 +69,32 @@ class Moderation(commands.Cog):
       await ctx.send(f"Muted {member.name}#{member.discriminator}.")
       await member.send(f"You were muted in the server **{guild.name}** for **{reason}**")
 
-    @commands.command(help="Unmutes a user.")
+    @commands.command(help="Unmutes a user.", hidden=True)
     @commands.is_owner()
     async def unmute(self, ctx, member: commands.MemberConverter):
       mutedRole = discord.utils.get(ctx.guild.roles, name="Redacted | Muted")
       await member.remove_roles(mutedRole)
       await ctx.send(f"Unmuted {member.name}#{member.discriminator}.")
       
-    @commands.command(help="Bans a member.")
+    @commands.command(help="A minimal ban comand to ban users.")
     @commands.is_owner()
     async def ban(self, ctx, member: commands.MemberConverter, *, reason=None):
-      await member.ban(reason=reason)
-      await ctx.send(f"{member.name}#{member.discriminator} was banned.")	
-      
-    @commands.command(help="Unbans a member.")
-    @commands.is_owner()
-    async def unban(self, ctx, *, member):
-      bannedUsers = await ctx.guild.bans()
-      name, discriminator = member.split("#")
-      
-      for ban in bannedUsers:
-        user = ban.user
+      await ctx.guild.ban(member, reason=reason)
+      embed=discord.Embed(title='🔨 Member Unbanned', description=f'{member.mention} has been banned!', color = discord.Color.red())
+      await ctx.send(embed=embed)
+
+    @commands.command(help='A simple unban command to unban users.')
+    @commands.has_guild_permissions(ban_members=True)
+    async def unban(self, ctx, target: discord.User, *, reason:str = None):
+      try:
+        await ctx.guild.fetch_ban(target)
+      except discord.NotFound:
+        return await ctx.send(embed=discord.Embed(title='Error', description='That user is not banned.'))
         
-        if(user.name, user.discriminator) == (name, discriminator):
-          await ctx.guild.unban(user)
-          await ctx.send(f"{user.name}#{user.discriminator} was unbanned.")
-          return	
+      embed=discord.Embed(title='🛠️ Member Unbanned', description=f'{target.mention} has been unbanned!', color = discord.Color.green())
+
+      await ctx.guild.unban(target, reason=reason)
+      await ctx.send(embed=embed)
 
 def setup(bot):
    bot.add_cog(Moderation(bot))
