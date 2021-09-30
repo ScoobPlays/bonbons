f05595098581f23ee8a77c72e9f90f5bf3456b4e
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-09-30 11:46:51 +0800

    Update main.py
    default help command made

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import discord #imports
+import discord 
 import os
-import platform
-import random
 from discord.ext import commands
 from datetime import datetime
 from keep_alive import keep_alive
@@ -9,14 +7,11 @@ from keep_alive import keep_alive
 # gold = #fff1b6
 # light_blue = #96daff
 
-
 token = os.environ['token']
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'), case_insensitive=True, intents = discord.Intents.all())
-bot.remove_command("help")
-bot.launch_time = datetime.utcnow() #variables & etc
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'), case_insensitive=True, intents = discord.Intents.all(), allowed_mentions=discord.AllowedMentions(everyone=False))
 
 @bot.event
-async def on_ready(): #events
+async def on_ready():
   print(f'Bot is ready to be used! Ping: {round(bot.latency * 1000)}')
   await bot.change_presence(status=discord.Status.dnd)
 
@@ -24,24 +19,34 @@ async def on_ready(): #events
     if filename.endswith('.py'):
       bot.load_extension(f'cogs.{filename[:-3]}')
 
-@bot.command(aliases=['ping', 'uptime']) #stats command for ping & uptime
-async def stats(ctx):
+class MyNewHelp(commands.MinimalHelpCommand):
+    async def send_pages(self):
+      destination = self.get_destination()
+      for page in self.paginator.pages:
+        embed = discord.Embed(description=page)
+        await destination.send(embed=embed)
+    
+    async def send_command_help(self, command):
+      embed = discord.Embed(title=self.get_command_signature(command))
+      embed.add_field(name="Help", value=command.help)
+      command_has_alias = command.aliases
+      command_has_help = command.help
+      if command_has_help:
+        embed.add_field(name="Help", value=command.help)
+      if command_has_alias:
+        embed.add_field(name="Aliases", value=", ".join(command.aliases), inline=False)
 
-  delta_uptime = datetime.utcnow() - bot.launch_time
-  hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
-  minutes, seconds = divmod(remainder, 60)
-  days, hours = divmod(hours, 24)
+      channel = self.get_destination()
+      await channel.send(embed=embed)
+    
+    async def send_error_message(self, error):
+      embed = discord.Embed(title="Error", description=error)
+      channel = self.get_destination()
+      await channel.send(embed=embed)
 
-  embed=discord.Embed(title='Bot Statistics')
-  embed.add_field(name='Uptime/Latency', value=f'`{hours}h, {minutes}m, {seconds}s` `{round(bot.latency * 1000)}ms`', inline=False)
-  embed.add_field(name='Python', value=f"`{platform.python_version()}`")
-  embed.add_field(name='Module', value=f"`Py-Cord 2.0`")
-  embed.timestamp = datetime.utcnow()
-  await ctx.send(embed=embed)
-
-keep_alive() #Keep alive method for replit ^^
-os.environ.setdefault('JISHAKU_NO_UNDERSCORE', '1') #Jishaku envs
+bot.help_command = MyNewHelp()
+keep_alive()
+os.environ.setdefault('JISHAKU_NO_UNDERSCORE', '1')
 os.environ.setdefault('JISHAKU_HIDE', '1')
 bot.load_extension('jishaku')
-
-bot.run(token) #running the bot
+bot.run(token)
