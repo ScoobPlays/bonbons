8a0c33688a9ce5d82e163433cb34d3c1f2975d1d
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-10-05 16:21:55 +0800

    moved commands to cogs

--- main.py
+++ main.py
@@ -13,45 +13,20 @@ from contextlib import redirect_stdout
 # light_blue = #96daff
 
 token = os.environ['token']
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'), case_insensitive=True, intents = discord.Intents.all(), allowed_mentions=discord.AllowedMentions(everyone=False))
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'), case_insensitive=True, intents = discord.Intents.all(), allowed_mentions=discord.AllowedMentions(everyone=False, roles=False))     
 
 @bot.event
 async def on_ready():
+
   print(f'Bot is ready to be used! Ping: {round(bot.latency * 1000)}')
-  await bot.change_presence(status=discord.Status.dnd)
+  channel = bot.get_channel(893419641953734667)
+  await channel.send(embed=discord.Embed(title='Bot Is Online', description=f"Ping: {int(bot.latency*1000)}ms"))
 
   for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
       bot.load_extension(f'cogs.{filename[:-3]}')
 
-class MyNewHelp(commands.MinimalHelpCommand):
-    async def send_pages(self):
-      destination = self.get_destination()
-      for page in self.paginator.pages:
-        embed = discord.Embed(description=page)
-        embed.timestamp=datetime.utcnow()
-        embed.set_author(name=self.context.author, icon_url=self.context.author.display_avatar)
-        embed.set_footer(text='Minimal Help Command', icon_url=self.context.author.display_avatar)
-        await destination.send(embed=embed)
-    
-    async def send_command_help(self, command):
-      embed = discord.Embed(title=self.get_command_signature(command))
-      command_has_alias = command.aliases
-      command_has_help = command.help
-      if command_has_help:
-        embed.add_field(name="Help", value=command.help)
-      if command_has_alias:
-        embed.add_field(name="Aliases", value=", ".join(command.aliases), inline=False)
-
-      channel = self.get_destination()
-      await channel.send(embed=embed)
-    
-    async def send_error_message(self, error):
-      embed = discord.Embed(title="Error", description=error)
-      channel = self.get_destination()
-      await channel.send(embed=embed)
-
-@bot.command(aliases=['eval', 'evaluate'])
+@bot.command(hidden=True, aliases=['eval', 'evaluate'])
 async def run(ctx, *, code):
     """Evaluates python code."""
     env = {
@@ -109,7 +84,7 @@ async def run(ctx, *, code):
     try:
         with redirect_stdout(stdout):
             ret = await func()
-    except Exception as e:
+    except Exception:
         value = stdout.getvalue()
         err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
     else:
@@ -144,9 +119,10 @@ async def run(ctx, *, code):
     else:
         await ctx.message.add_reaction('\u2705')
 
-bot.help_command = MyNewHelp()
 keep_alive()
 os.environ.setdefault('JISHAKU_NO_UNDERSCORE', '1')
 os.environ.setdefault('JISHAKU_HIDE', '1')
 bot.load_extension('jishaku')
-bot.run(token)
+
+if __name__ == "__main__":
+  bot.run(token)
