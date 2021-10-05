837c19941e2a629b7eec928e3e7eb49e13a76a33
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-10-05 15:33:25 +0800

    Update moderation.py
    removed mute/unmute commands

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -18,13 +18,13 @@ class Moderation(commands.Cog):
       try:
        await member.edit(nick=nick)
        embed=discord.Embed(description = f'You have changed {member.mention}\'s nick.', color=discord.Color.green())
-       embed.set_author(name=f'{ctx.author.display_name}', icon_url = ctx.author.avatar.url)
+       embed.set_author(name=f'{ctx.author}', icon_url = ctx.author.display_avatar)
        embed.timestamp = datetime.utcnow()
        await ctx.send(embed=embed)
      
       except: 
        embed=discord.Embed(description = f'I can\'t change {member.mention}\'s nick.', color=discord.Color.red())
-       embed.set_author(name=f'{ctx.author.display_name}', icon_url = ctx.author.avatar.url)
+       embed.set_author(name=f'{ctx.author}', icon_url = ctx.author.display_avatar)
        embed.timestamp = datetime.utcnow() 
        await ctx.send(embed=embed)
 
@@ -38,44 +38,17 @@ class Moderation(commands.Cog):
     async def role(self, ctx, member: commands.MemberConverter=None, *, role: commands.RoleConverter):
 
       if role in member.roles:
-
         await member.remove_roles(role)
         embed=discord.Embed(title='Role removed!', description=f'Removed {role.mention} from {member.mention}.', color=discord.Color.red())
         embed.timestamp=datetime.utcnow()
         await ctx.send(embed=embed)
-        
+      
       else:
         await member.add_roles(role)
         embed=discord.Embed(title='Role added!', description=f'Added {role.mention} to {member.mention}.', color=discord.Color.green())
         embed.timestamp=datetime.utcnow()
         await ctx.send(embed=embed)
 
-    ######################
-    #  Removed Commands  #
-    ######################
-
-    @commands.command(help="Mutes the specified user!", hidden=True)
-    @commands.is_owner()
-    async def mute(self,ctx, member: commands.MemberConverter, *, reason=None):
-      guild = ctx.guild
-      mutedRole = discord.utils.get(guild.roles, name="Redacted | Muted")
-      
-      if not mutedRole:
-        mutedRole = await guild.create_role(name="Redacted | Muted")
-      for channel in guild.channels:
-        await channel.set_permissions(mutedRole, speak=False, send_messages=False, read_message_history=True, read_messages=False)
-        
-      await member.add_roles(mutedRole, reason=reason)
-      await ctx.send(f"Muted {member.name}#{member.discriminator}.")
-      await member.send(f"You were muted in the server **{guild.name}** for **{reason}**")
-
-    @commands.command(help="Unmutes a user.", hidden=True)
-    @commands.is_owner()
-    async def unmute(self, ctx, member: commands.MemberConverter):
-      mutedRole = discord.utils.get(ctx.guild.roles, name="Redacted | Muted")
-      await member.remove_roles(mutedRole)
-      await ctx.send(f"Unmuted {member.name}#{member.discriminator}.")
-      
     @commands.command(help="A minimal ban comand to ban users.")
     @commands.is_owner()
     async def ban(self, ctx, member: commands.MemberConverter, *, reason=None):
