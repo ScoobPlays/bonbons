eb7f9c516d3fa230276276aa2982c39fa6e75e94
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-10-10 18:26:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import discord 
-import os
-from discord.ext import commands
-from datetime import datetime
+import discord, os, inspect, io
+from discord.ext import commands, tasks
+#from datetime import datetime
 from keep_alive import keep_alive
-import inspect
-import io
-import json
 import textwrap
 import traceback
 from contextlib import redirect_stdout
@@ -25,7 +21,57 @@ bot = commands.Bot(
 async def on_ready():
   print(f'Bot is ready to be used! Ping: {round(bot.latency * 1000)}')
 
+@tasks.loop(seconds=1000)
+async def member_stats():
+  guild = bot.get_guild(880030618275155998)
+
+  online_channel = guild.get_channel(895606651728584725)
+  offline_channel = guild.get_channel(895606862437826641)
+  idle_channel = guild.get_channel(895606765142556722)
+  dnd_channel = guild.get_channel(895606807093985301)
+
+  online_members = 0
+  offline_members = 0
+  idle_members = 0
+  dnd_members = 0
+
+  for member in guild.members:
+
+    if member.status == discord.Status.online:
+      online_members += 1
+      await online_channel.edit(name=f'Online: {online_members}')
+
+    if member.status == discord.Status.offline:
+      offline_members += 1
+      await offline_channel.edit(name=f'Offline: {offline_members}')
+
+
+    if member.status == discord.Status.idle:
+      idle_members += 1
+      await idle_channel.edit(name=f'Idle: {idle_members}')
+
+    if member.status == discord.Status.dnd:
+      dnd_members += 1
+      await dnd_channel.edit(name=f'DnD: {dnd_members}')
+
+@member_stats.before_loop
+async def before_ms():
+  await bot.wait_until_ready()
+
+member_stats.start()
+
+@bot.command(hidden=True, aliases=['rf'], description="Refreshes the bot.")
+@commands.is_owner()
+async def refresh(ctx):
+  try:
+    member_stats.start()
+  except Exception as e:
+    print(e)
+  finally:
+    member_stats.start()
+
 @bot.command(hidden=True, aliases=['eval', 'evaluate'])
+@commands.is_owner()
 async def run(ctx, *, code):
     """Evaluates python code."""
     env = {
