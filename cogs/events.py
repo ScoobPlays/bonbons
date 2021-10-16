8072419372825c8ab8e944b5cd9c4abbf0eece90
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-10-16 22:10:03 +0800

    COOLER highlights..
    😎

--- cogs/events.py
+++ cogs/events.py
@@ -5,6 +5,33 @@ from discord.ext import commands
 class Events(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
+    self.badwords = ["kayle", "carrot", "geno"]
+    
+  def hw_append(self, value):
+    self.badwords.append(value)
+
+  @commands.Cog.listener() #highlights event
+  async def on_message(self, msg):
+  
+    if msg.author == self.bot.user:
+      return
+
+    offsprings = self.bot.get_channel(880030618275156001)
+
+    for word in self.badwords:
+      if word in msg.content.lower():
+        embed=discord.Embed(title=f'In {msg.guild.name}, "{word}" got highlighted.', description=f'[Jump to message!]({msg.jump_url})')
+        embed.add_field(name=f'Message', value=msg.content)
+        embed.set_footer(text=f'From {msg.author}')
+        await offsprings.send(embed=embed)
+
+  @commands.command()
+  async def append(self, ctx, value):
+
+    self.hw_append(value)
+    highlights_list = ", ".join(self.badwords)
+    embed=discord.Embed(title='Current List', description=f'{highlights_list}')
+    await ctx.send(f"Appended \"{value}\" to the highlights.", embed=embed)
 
   @commands.Cog.listener()
   async def on_member_join(self, member):
