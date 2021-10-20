f186e401cb3f4c277b4c927253bd896fec80723c
Kraots <nagit.robermessi@gmail.com>
2021-10-20 14:47:47 +0300

    Removed unused imports

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-from datetime import datetime
 import base64
 import aiohttp
 
--- cogs/owner.py
+++ cogs/owner.py
@@ -4,7 +4,6 @@ import os
 import sys
 from datetime import datetime
 import platform
-import os
 import inspect
 import io
 import textwrap
