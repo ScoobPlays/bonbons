adeef7f66d05d82a00ad62b87810cfc9b4060855
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-10-20 21:23:05 +0800

    `pycord` => `disnake` & ⭐black⭐
    added slash commands to `dog`, `say`, & `cat`. ⭐

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,5 +1,5 @@
-import discord
-from discord.ext import commands
+import disnake
+from disnake.ext import commands
 import base64
 import aiohttp
 
@@ -13,15 +13,15 @@ class Misc(commands.Cog):
         message_bytes = text.encode("ascii")
         base64_bytes = base64.b64encode(message_bytes)
         base64_message = base64_bytes.decode("ascii")
-        embed = discord.Embed(
-            title="✅ Message Was Encoded", color=discord.Color.green()
+        embed = disnake.Embed(
+            title="✅ Message Was Encoded", color=disnake.Color.green()
         )
         embed.add_field(name="Output:", value=base64_message)
         await ctx.send(embed=embed)
 
     @encode.error
     async def encode_error(self, ctx, error):
-        embed = discord.Embed(
+        embed = disnake.Embed(
             title="Encoding Error", description="Sorry, I couldn't encode that message."
         )
         await ctx.send(embed=embed)
@@ -31,15 +31,15 @@ class Misc(commands.Cog):
         base64_bytes = text.encode("ascii")
         message_bytes = base64.b64decode(base64_bytes)
         message = message_bytes.decode("ascii")
-        embed = discord.Embed(
-            title="✅ Message Was Encoded", color=discord.Color.green()
+        embed = disnake.Embed(
+            title="✅ Message Was Encoded", color=disnake.Color.green()
         )
         embed.add_field(name="Output:", value=message)
         await ctx.send(embed=embed)
 
     @decode.error
     async def decode_error(self, ctx, error):
-        embed = discord.Embed(
+        embed = disnake.Embed(
             title="Decoding Error", description="Sorry, I couldn't decode that message."
         )
         await ctx.send(embed=embed)
@@ -47,6 +47,10 @@ class Misc(commands.Cog):
     @commands.command(help="Says whatever you want for you!")
     async def say(self, ctx, *, message):
         await ctx.send(message)
+
+    @commands.slash_command()
+    async def say(self, inter, message):
+        await inter.response.send_message(message)
 
     """Animals"""
 
@@ -57,8 +61,18 @@ class Misc(commands.Cog):
             async with session.get("http://aws.random.cat/meow") as r:
                 if r.status == 200:
                     js = await r.json()
-                    embed = discord.Embed().set_image(url=js["file"])
+                    embed = disnake.Embed().set_image(url=js["file"])
                     await ctx.send(embed=embed)
+
+    @commands.slash_command()
+    async def cat(self, inter):
+        """Sends a random cat image"""
+        async with aiohttp.ClientSession() as session:
+            async with session.get("http://aws.random.cat/meow") as r:
+                if r.status == 200:
+                    js = await r.json()
+                    embed = disnake.Embed().set_image(url=js["file"])
+                    await inter.response.send_message(embed=embed, ephemeral=False)
 
     @commands.command()
     async def dog(self, ctx):
@@ -67,8 +81,18 @@ class Misc(commands.Cog):
             async with session.get("https://dog.ceo/api/breeds/image/random") as r:
                 if r.status == 200:
                     js = await r.json()
-                    embed = discord.Embed().set_image(url=js["message"])
+                    embed = disnake.Embed().set_image(url=js["message"])
                     await ctx.send(embed=embed)
+
+    @commands.command()
+    async def dog(self, inter):
+        """Sends a random dog image"""
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://dog.ceo/api/breeds/image/random") as r:
+                if r.status == 200:
+                    js = await r.json()
+                    embed = disnake.Embed().set_image(url=js["message"])
+                    await inter.response.send_message(embed=embed, ephemeral=False)
 
 
 def setup(bot):
