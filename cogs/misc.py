3fe730cab57283d3eb0442cbb4d7232a1c5f757b
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-10-26 21:24:50 +0800

    moved commands &  black

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,6 +1,5 @@
 import disnake
 from disnake.ext import commands
-import base64
 import aiohttp
 from urllib.parse import quote_plus
 
@@ -17,49 +16,15 @@ class Misc(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def encode(self, ctx: commands.Context, *, body:str):
-        message_bytes = body.encode("ascii")
-        base64_bytes = base64.b64encode(message_bytes)
-        base64_message = base64_bytes.decode("ascii")
-        embed = disnake.Embed(
-            title="✅ Message Was Encoded", color=disnake.Color.green()
-        )
-        embed.add_field(name="Output:", value=base64_message)
-        await ctx.send(embed=embed)
-
-    @encode.error
-    async def encode_error(self, ctx: commands.Context, error):
-        embed = disnake.Embed(
-            title="Encoding Error", description="Sorry, I couldn't encode that message."
-        )
-        await ctx.send(embed=embed)
-
-    @commands.command()
-    async def decode(self, ctx: commands.Context, *, body:str):
-        base64_bytes = body.encode("ascii")
-        message_bytes = base64.b64decode(base64_bytes)
-        message = message_bytes.decode("ascii")
-        embed = disnake.Embed(
-            title="✅ Message Was Encoded", color=disnake.Color.green()
-        )
-        embed.add_field(name="Output:", value=message)
-        await ctx.send(embed=embed)
-
-    @decode.error
-    async def decode_error(self, ctx: commands.Context, error):
-        embed = disnake.Embed(
-            title="Decoding Error", description="Sorry, I couldn't decode that message."
-        )
-        await ctx.send(embed=embed)
-
-    @commands.command(name="say", help="Says whatever you want for you!")
-    async def say_cmd(self, ctx: commands.Context, *, message:str):
+    @commands.command(name="say", help="Says whatever you want for you")
+    async def say_cmd(self, ctx: commands.Context, *, message: str):
         await ctx.send(message)
 
     @commands.slash_command(name="say")
-    async def say_slash(self, inter: disnake.ApplicationCommandInteraction, message:str):
-        "Says whatever you want for you!"
+    async def say_slash(
+        self, inter: disnake.ApplicationCommandInteraction, message: str
+    ):
+        "Says whatever you want for you"
         await inter.response.send_message(message, ephemeral=False)
 
     """Animals"""
@@ -106,12 +71,14 @@ class Misc(commands.Cog):
 
     @commands.command(name="google")
     async def google_cmd(self, ctx: commands.Context, *, query: str):
-        """Returns a google link for a query."""
+        """Returns a google link for a query"""
         await ctx.send(f"Google Result for: `{query}`", view=Google(query))
 
     @commands.slash_command(name="google")
-    async def google_slash(self, inter: disnake.ApplicationCommandInteraction, query: str):
-        """Returns a google link for a query."""
+    async def google_slash(
+        self, inter: disnake.ApplicationCommandInteraction, query: str
+    ):
+        """Returns a google link for a query"""
         await inter.response.send_message(
             f"Google Result for: `{query}`", view=Google(query), ephemeral=False
         )
