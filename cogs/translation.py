222f5c8f416bdcb10bd2675bec00a1320e87711f
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-10-29 21:25:49 +0800

    prefixed commands > slash commands

--- cogs/translation.py
+++ cogs/translation.py
@@ -1,43 +1,38 @@
-import disnake
 from disnake.ext import commands
-import base64
+from utils.translation import b64_encode, b64_decode
 
-#def binary(text: str):
-#  return (" ".join(f"{ord(i):08b}" for i in text))
 
-#https://www.rapidtables.com/convert/number/binary-to-ascii.html
-
-class Translation(commands.Cog):
+class Translation(
+    commands.Cog,
+    description="Commands that can translate something into another language",
+):
     def __init__(self, bot):
-      self.bot = bot
+        self.bot = bot
 
-    @commands.group()
-    async def base64(self, ctx: commands.Context):
-      pass
+    @commands.slash_command()
+    async def base64(self, inter):
+        pass
 
-    @base64.command()
-    async def encode(self, ctx: commands.Context, *, text: str):
-        """Encodes a message"""
+    @base64.sub_command()
+    async def encode(self, inter, text: str):
+        """Encodes a message into a base64 string"""
         try:
-          message_bytes = text.encode("ascii")
-          base64_bytes = base64.b64encode(message_bytes)
-          base64_message = base64_bytes.decode("ascii")
-      
-          await ctx.reply(base64_message, mention_author=False)
+            await inter.response.send_message(b64_encode(text), ephemeral=False)
         except Exception:
-          await ctx.reply(f"Couldn't encode that message.", mention_author=False)
+            await inter.response.send_message(
+                f"Couldn't encode that message.", ephemeral=False
+            )
 
-    @base64.command()
-    async def decode(self, ctx: commands.Context, *, body:str):
-        """Decodes base64"""
+    @base64.sub_command()
+    async def decode(self, inter, text: str):
+        """Decodes a base64 string"""
         try:
-          base64_bytes = body.encode("ascii")
-          message_bytes = base64.b64decode(base64_bytes)
-          message = message_bytes.decode("ascii")
-          
-          await ctx.reply(message, mention_author=False)
+            await inter.response.send_message(b64_decode(text), ephemeral=False)
         except Exception:
-          await ctx.reply("Couldn't decode that message.", mention_author=False)
+            await inter.response.send_message(
+                "Couldn't decode that message.", ephemeral=False
+            )
+
 
 def setup(bot):
-  bot.add_cog(Translation(bot))
+    bot.add_cog(Translation(bot))
