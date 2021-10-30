b97082df8a01ff4038177d6bf0093d76ccad03e1
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-10-30 19:50:14 +0800

    hehe

--- main.py
+++ main.py
@@ -3,16 +3,5 @@ from utils.bonbons import Bonbons
 
 bot = Bonbons()
 
-for filename in os.listdir("./cogs"):
-    if filename.endswith(".py"):
-        bot.load_extension(f"cogs.{filename[:-3]}")
-
-##jishaku/https://github.com/Kraots/jishaku <3
-os.environ["JISHAKU_FORCE_PAGINATOR"] = "1"
-os.environ["JISHAKU_EMBEDDED_JSK"] = "1"
-os.environ.setdefault("JISHAKU_NO_UNDERSCORE", "1")
-os.environ.setdefault("JISHAKU_HIDE", "1")
-bot.load_extension("jishaku")
-
 if __name__ == "__main__":
     bot.run(os.environ["token"])
