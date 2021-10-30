504bab1b19bae09038b32e07b5240f15fc454e48
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-10-30 19:52:41 +0800

    when the async

--- utils/translation.py
+++ utils/translation.py
@@ -1,12 +1,12 @@
 import base64
 
-def b64_encode(text: str):
+async def b64_encode(text: str):
     message_bytes = text.encode("ascii")
     base64_bytes = base64.b64encode(message_bytes)
     message = base64_bytes.decode("ascii")
     return message
 
-def b64_decode(text: str):
+async def b64_decode(text: str):
     b64msg = text.encode("ascii")
     message_bytes = base64.b64decode(b64msg)
     message = message_bytes.decode("ascii")
