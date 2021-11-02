108774bdbeed57ebe3d8a93986505d554ae9f981
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-11-02 22:50:26 +0800

    Update and rename misc.py to images.py

--- cogs/images.py
+++ cogs/images.py
@@ -1,33 +1,11 @@
 import disnake
 from disnake.ext import commands
 import aiohttp
-from urllib.parse import quote_plus
 
 
-class Google(disnake.ui.View):
-    def __init__(self, query: str):
-        super().__init__()
-        query = quote_plus(query)
-        url = f"https://www.google.com/search?q={query}"
-        self.add_item(disnake.ui.Button(label="Click Here", url=url))
-
-
-class Misc(commands.Cog, description="Very weird commands"):
+class Images(commands.Cog, description="Image related commands."):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command(name="say", help="Says whatever you want for you")
-    async def say(self, ctx: commands.Context, *, argument: str):
-        await ctx.send(argument)
-
-    @commands.slash_command(name="say")
-    async def say_slash(
-        self, inter: disnake.ApplicationCommandInteraction, argument: str
-    ):
-        "Says whatever you want for you"
-        await inter.response.send_message(argument, ephemeral=False)
-
-    """Animals"""
 
     @commands.command(name="cat")
     async def cat(self, ctx: commands.Context):
@@ -69,23 +47,6 @@ class Misc(commands.Cog, description="Very weird commands"):
                     embed = disnake.Embed().set_image(url=js["message"])
                     await inter.response.send_message(embed=embed, ephemeral=False)
 
-    @commands.command(name="google")
-    async def google(self, ctx: commands.Context, *, query: str):
-
-        """Returns a google link for a query"""
-
-        await ctx.send(f"Google Result for: `{query}`", view=Google(query))
-
-    @commands.slash_command(name="google")
-    async def google_slash(
-        self, inter: disnake.ApplicationCommandInteraction, query: str
-    ):
-        """Returns a google link for a query"""
-
-        await inter.response.send_message(
-            f"Google Result for: `{query}`", view=Google(query), ephemeral=False
-        )
-
 
 def setup(bot):
-    bot.add_cog(Misc(bot))
+    bot.add_cog(Images(bot))
