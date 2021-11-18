8244285e0b07415838e6cbb1a70c0fe4552562a7
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-11-18 13:46:13 +0000

    editing tags command

--- cogs/tags.py
+++ cogs/tags.py
@@ -2,14 +2,73 @@ import disnake
 from disnake.ext import commands
 from datetime import datetime
 from utils.mongo import cluster
+from utils.bonbons import Bonbons
+
+bot = Bonbons()
+
+
+class Buttons(disnake.ui.View):
+    def __init__(self, bot, inter, name):
+        super().__init__()
+        self.bot = bot
+        self.name = name
+        self.db = cluster["discord"]
+        self.inter = inter
+        self.author = self.inter.author
+        self.edit = self.inter.edit_original_message
+
+    async def interaction_check(self, inter) -> bool:
+        if inter.author.id != self.author.id:
+            await inter.response.send_message(
+                f"Only `{self.author.mention}` can use the buttons on this message.",
+                ephemeral=True,
+            )
+            return False
+        return True
+
+    @disnake.ui.button(label="Content")
+    async def tag_content(self, button, inter):
+        self.tags = self.db[str(inter.guild.id)]
+
+        await inter.response.send_message(
+            "Alright so what do you want the change this tag's content to?",
+            ephemeral=True,
+        )
+
+        msg = await self.bot.wait_for(
+            "message", check=lambda m: m.author.id == inter.author.id
+        )
+        await msg.add_reaction("✅")
+        await self.tags.update_one(self.name, {"$set": {"content": msg.content}})
+
+        await self.edit(
+            embed=disnake.Embed(
+                title="Tag was Edited",
+                description="Tag was successfully edited!",
+                color=disnake.Color.green(),
+            ),
+            view=None,
+        )
+
 
 class Tags(commands.Cog, description="Commands related to tags."):
     def __init__(self, bot):
         self.bot = bot
         self.db = cluster["discord"]
 
-    async def tags_autocomp(inter, input: str) -> str:
+    async def inter_autocomp(inter, input: str) -> str:
         db_tags = cluster["discord"][str(inter.guild.id)]
+        find_tags = await db_tags.find({}).to_list(10000)
+
+        all_tags = []
+
+        for tags in find_tags:
+            all_tags.append(tags["name"])
+
+        return [tag for tag in all_tags if input.lower() in tag.lower()]
+
+    async def context_autocomp(ctx, input: str) -> str:
+        db_tags = cluster["discord"][str(ctx.guild.id)]
         find_tags = await db_tags.find({}).to_list(10000)
 
         all_tags = []
@@ -71,12 +130,46 @@ class Tags(commands.Cog, description="Commands related to tags."):
 
     @tag.sub_command()
     @commands.guild_only()
+    async def edit(
+        self,
+        inter,
+        name: str = commands.Param(
+            description="The name of the tag", autocomp=inter_autocomp
+        ),
+    ):
+        """Edit's a tag you own"""
+        self.tags = self.db[str(inter.guild.id)]
+
+        tag_data = await self.tags.find_one({"name": name})
+        tag_owner = tag_data["owner"]
+
+        if not tag_data:
+            return await inter.response.send_message(
+                "A tag with this name does not exist.", ephemeral=True
+            )
+
+        if tag_owner != inter.author.id:
+            return await inter.response.send_message(
+                "You do not own this tag.", ephemeral=False
+            )
+
+        await inter.response.send_message(
+            embed=disnake.Embed(
+                title="Editing a Tag",
+                description="Click the button below to edit your tag.",
+                color=disnake.Color.greyple(),
+            ),
+            view=Buttons(self.bot, inter, tag_data),
+        )
+
+    @tag.sub_command()
+    @commands.guild_only()
     @commands.has_permissions(kick_members=True)
     async def delete(
         self,
         inter: disnake.ApplicationCommandInteraction,
         name: str = commands.param(
-            description="The tag's name to delete", autocomp=tags_autocomp
+            description="The tag's name to delete", autocomp=inter_autocomp
         ),
     ) -> None:
         """Delete's a tag"""
@@ -103,7 +196,7 @@ class Tags(commands.Cog, description="Commands related to tags."):
         self,
         inter: disnake.ApplicationCommandInteraction,
         name: str = commands.param(
-            description="The tag's name to display", autocomp=tags_autocomp
+            description="The tag's name to display", autocomp=inter_autocomp
         ),
     ) -> None:
         """Display's a tag"""
@@ -121,7 +214,7 @@ class Tags(commands.Cog, description="Commands related to tags."):
         self,
         inter: disnake.ApplicationCommandInteraction,
         name: str = commands.param(
-            description="The tag's name", autocomp=tags_autocomp
+            description="The tag's name", autocomp=inter_autocomp
         ),
     ) -> None:
         """Gives information about a tag"""
