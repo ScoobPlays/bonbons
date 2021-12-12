20fadc2b89b916627640a2eed154a4164adc8381
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-12-12 13:50:43 +0000

    refactor: starboard

--- cogs/starboard.py
+++ cogs/starboard.py
@@ -1,5 +1,5 @@
 from disnake.ext import commands
-from disnake import Embed, Color
+import disnake
 from utils.env import starboard, config
 from datetime import datetime
 
@@ -14,15 +14,20 @@ class Starboard(commands.Cog, description="Starboard related commands."):
 
         try:
 
-            data = await config.find_one(
-                {"reaction_count": "The reaction count for the starboard."}
-            )
-            values = {"$set": {"reactions": count}}
+            data = await config.find_one({"_id": ctx.guild.id})
 
-            await config.update_one(data, values)
-            await ctx.send(
-                f"I have set the reactions needed for the starboard to `{count}`."
-            )
+            if not data:
+                return await ctx.send(
+                    "This server has no starboard channel yet. Do `.starboard set #channel`"
+                )
+
+            if data:
+                await config.update_one(
+                    {"_id": ctx.guild.id}, {"$set": {"reactions": count}}
+                )
+                return await ctx.send(
+                    f"I have set the reactions needed for the starboard to `{count}`."
+                )
 
         except Exception as e:
             print(e)
@@ -31,60 +36,54 @@ class Starboard(commands.Cog, description="Starboard related commands."):
 
         """Adds something to the starboard"""
 
-        try:
-            if reaction.message.embeds:
-                return
+        if reaction.message.embeds:
+            return
 
-            reactions = await config.find_one(
-                {"reaction_count": "The reaction count for the starboard."}
+        reactions = await config.find_one({"_id": reaction.message.guild.id})
+        starboard_channel_id = reactions["channel"]
+        starboard_channel = self.bot.get_channel(
+            starboard_channel_id
+        ) or await self.bot.fetch_channel(starboard.channel_id)
+
+        data = await starboard.find_one({"_id": reaction.message.id})
+        if data:
+            data_channel = data["channel"]
+
+            msg = self.bot.get_message(
+                data["starboard_message"]
+            ) or await self.bot.user.fetch_message(data["starboard_message"])
+            await msg.edit(
+                content=f"⭐ **{reaction.count}** <#{data_channel}> ID: {data['_id']}"
             )
 
-            data = await starboard.find_one({"_id": reaction.message.id})
-            starboard_channel = self.bot.get_channel(
-                910404322947387422
-            ) or await self.bot.fetch_channel(910404322947387422)
+        if not data:
+            if reaction.emoji == "⭐" and reaction.count > reactions["reactions"]:
 
-            if data:
-                data_channel = data["channel"]
-
-                msg = self.bot.get_message(
-                    data["starboard_message"]
-                ) or await self.bot.user.fetch_message(data["starboard_message"])
-                await msg.edit(
-                    content=f"⭐ **{reaction.count}** <#{data_channel}> ID: {data['_id']}"
+                em = disnake.Embed(
+                    description=reaction.message.content,
+                    color=disnake.Color.greyple(),
+                    timestamp=datetime.utcnow(),
+                )
+                em.set_author(
+                    name=reaction.message.author,
+                    icon_url=reaction.message.author.display_avatar,
                 )
 
-            if not data:
-                if reaction.emoji == "⭐" and reaction.count > reactions["reactions"]:
-
-                    em = Embed(
-                        description=reaction.message.content,
-                        color=Color.greyple(),
-                        timestamp=datetime.utcnow(),
-                    )
-                    em.set_author(
-                        name=reaction.message.author,
-                        icon_url=reaction.message.author.display_avatar,
-                    )
-
-                    if reaction.message.attachments:
-                        em.set_image(url=reaction.message.attachments[0].url)
-                    bot_msg = await starboard_channel.send(
-                        content=f"⭐ **{reaction.count}** <#{reaction.message.channel.id}> ID: {reaction.message.id}",
-                        embed=em,
-                    )
-                    await starboard.insert_one(
-                        {
-                            "_id": reaction.message.id,
-                            "channel": reaction.message.channel.id,
-                            "author": reaction.message.author.id,
-                            "content": reaction.message.content,
-                            "starboard_message": bot_msg.id,
-                        }
-                    )
-
-        except Exception as e:
-            print(e)
+                if reaction.message.attachments:
+                    em.set_image(url=reaction.message.attachments[0].url)
+                bot_msg = await starboard_channel.send(
+                    content=f"⭐ **{reaction.count}** <#{reaction.message.channel.id}> ID: {reaction.message.id}",
+                    embed=em,
+                )
+                await starboard.insert_one(
+                    {
+                        "_id": reaction.message.id,
+                        "channel": reaction.message.channel.id,
+                        "author": reaction.message.author.id,
+                        "content": reaction.message.content,
+                        "starboard_message": bot_msg.id,
+                    }
+                )
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
@@ -96,15 +95,73 @@ class Starboard(commands.Cog, description="Starboard related commands."):
         await ctx.send_help("starboard")
 
     @starboard.command()
-    @commands.is_owner()
+    @commands.has_permissions(manage_channels=True)
     async def reactions(self, ctx, count: int):
-        """Sets the reactions needed for the starboard. (Default is 0)"""
+        """Sets the reactions needed for the starboard."""
+
+        if not ctx.author.guild_permissions.manage_channels:
+            return await ctx.send(
+                "You do not have enough permissions to set the starboard channel."
+            )
+
         await self.set_starboard_count(ctx, count)
 
     @starboard.command()
     async def show(self, ctx, message: int):
         """Shows a message that's been starboard'd"""
         ...
+
+    @starboard.command()
+    async def info(self, ctx):
+        """Display's the server's starboard information."""
+        data = await config.find_one({"_id": ctx.guild.id})
+        if data:
+            embed = disnake.Embed(
+                title="Starboard Information",
+                description=f"• **Channel:** <#{data['channel']}>\n• **Reactions Needed:** {data['reactions']}",
+                color=disnake.Color.greyple(),
+                timestamp=disnake.utils.utcnow(),
+            )
+
+            await ctx.send(embed=embed)
+        if not data:
+            await ctx.send("This server has no starboard setup yet.")
+
+    @starboard.command()
+    @commands.has_permissions(manage_channels=True)
+    async def set(self, ctx, channel: disnake.TextChannel):
+        """Sets a channel to be the starboard."""
+        data = await config.find_one(
+            {
+                "_id": ctx.guild.id,
+            }
+        )
+
+        if not ctx.author.guild_permissions.manage_channels:
+            return await ctx.send(
+                "You do not have enough permissions to set the starboard channel."
+            )
+
+        if data:
+            await config.update_one(
+                {"_id": ctx.guild.id}, {"$set": {"channel": channel.id}}
+            )
+            return await ctx.send(
+                f"I have set the channel for the starboard to {channel.mention}"
+            )
+
+        if not data:
+            await config.insert_one(
+                {
+                    "_id": ctx.guild.id,
+                    "channel": channel.id,
+                    "reactions": 5,
+                }
+            )
+
+            await ctx.send(
+                f"I have set the channel for the starboard to {channel.mention}"
+            )
 
 
 def setup(bot):
