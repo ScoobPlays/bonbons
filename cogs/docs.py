76b655e8731894ada81c0243bae77530f0d1aabb
eephe <84506518+sifte@users.noreply.github.com>
2022-02-03 10:14:55 +0800

    fix eol

--- cogs/docs.py
+++ cogs/docs.py
@@ -7,7 +7,7 @@ class MyCog(cog.Docs):
         super().__init__(bot)
         self.items = (
             ("disnake", "https://disnake.readthedocs.io/en/latest/"),
-            ("python", "https://docs.python.org/3/'),
+            ("python", "https://docs.python.org/3/"),
             ("aiohttp", "https://aiohttp.readthedocs.io/en/stable/"),
             ("nextcord", "https://nextcord.readthedocs.io/en/latest/"),
             ("pycord", "https://docs.pycord.dev/en/master/")
