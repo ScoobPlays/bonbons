be69e2b1eaa4694a6f8b995b013de2897af10c61
eephe <84506518+sifte@users.noreply.github.com>
2022-02-10 17:49:22 +0800

    use staticmethods

--- bot/cogs/general.py
+++ bot/cogs/general.py
@@ -22,13 +22,15 @@ class General(Cog, description="General commands."):
         self.emoji = "🙌"
         self.afk = self.bot.mongo["discord"]["afk"]
 
-    def b64_encode(self, text: str):
+    @staticmethod
+    def b64_encode(text: str):
         message_bytes = text.encode("ascii")
         base64_bytes = base64.b64encode(message_bytes)
         message = base64_bytes.decode("ascii")
         return message
 
-    def b64_decode(self, text: str):
+    @staticmethod
+    def b64_decode(text: str):
         b64msg = text.encode("ascii")
         message_bytes = base64.b64decode(b64msg)
         message = message_bytes.decode("ascii")
